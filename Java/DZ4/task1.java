103dd68b903669fb50280f7548f790708d5f3554
Leonid Tolstov <leonski@mail.ru>
2023-06-22 13:17:28 +0300

    DZ4

---
+++ Java/DZ4/task1.java
@@ -0,0 +1,45 @@
+// Даны два Deque, представляющие два целых числа. Цифры хранятся в обратном порядке и каждый из их узлов содержит одну цифру.
+// 1) Умножьте два числа и верните произведение в виде связанного списка.
+// 2) Сложите два числа и верните сумму в виде связанного списка. Одно или два числа могут быть отрицательными.
+
+// Даны два Deque, цифры в обратном порядке.
+// [3,2,1] - пример Deque
+// [5,4,3]- пример второго Deque
+// 1) 123 * 345 = 42 435
+// Ответ всегда - связный список, в обычном порядке
+// [4,2,4,3,5] - пример ответа
+
+package DZ4;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+public class task1 {
+    public static void main(String[] args) {
+        Deque<Integer> d1 = new ArrayDeque<>(Arrays.asList(1,2,3));
+        Deque<Integer> d2 = new ArrayDeque<>(Arrays.asList(5,4,5));
+        // result [6,6,0,1]
+        task1 task = new task1();
+         Deque<Integer> result = task.sum(d1, d2);
+        System.out.println(result);
+    }
+    public Deque<Integer> sum(Deque<Integer> d1, Deque<Integer> d2) {
+        Deque<Integer> result = new ArrayDeque<>();
+        int carry = 0;
+         while (!d1.isEmpty() || !d2.isEmpty()) { 
+            int sum = carry;
+            if (!d1.isEmpty()) {
+                sum += d1.removeFirst();
+            }
+            if (!d2.isEmpty()) {
+                sum += d2.removeFirst();
+            }
+            result.addLast(sum % 10);
+            carry = sum / 10; 
+        }
+        if (carry != 0) {
+            result.addLast(carry);
+        }
+        return result;
+    }
+}
